b2fc5eb347b837ad68fc27484b43037fb65776b1
Amir <amir.akhoundi85@gmail.com>
2023-10-17 13:42:09 +0300

    configuerd morgan for shoeing data

--- server.js
+++ server.js
@@ -10,7 +10,21 @@ const {
 const info = require("./controllers/infoController");
 
 // middlware
-app.use(morgan("tiny"));
+// app.use(morgan("tiny"));
+app.use(
+  morgan(function (tokens, req, res) {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, "content-length"),
+      "-",
+      tokens["response-time"](req, res),
+      "ms",
+      JSON.stringify(req.body),
+    ].join(" ");
+  })
+);
 app.use(express.json());
 
 // route ---> http request
